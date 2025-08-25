feat(DL_Track_US_GUI.py-/gui_helpers/filer_data.py-/gui_helpers/do_calculation_video.py-/gui_helpers/calculatearchitecture_video.py): Fixed fascicle intersions and MT calculation. Added tangential extrapolation op apos.

DL_Track_US/gui_helpers/do_calculations.py
@@ -195,7 +195,7 @@ def filter_fascicles(df: pd.DataFrame) -> pd.DataFrame:
     return df[df["keep"]].drop(columns=["keep"])
 
 
-def doCalculations(  # TODO adapt docstring
+def doCalculations(  
     original_image: np.ndarray,
     img_copy: np.ndarray,
     h: int,